adcdd2fb2cfd6675738e56b3f65a4900f9b6b29f
Tiwaloluwa Bakre <120737232+teawa-b@users.noreply.github.com>
2026-05-22 02:46:20 +0100

    feat: configure Vercel redirect to Traktrain and remove old files

--- pages/dashboard/AdminBeatPacks.tsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { useBeatPacks } from '@/contexts/BeatPackContext';
-const AdminBeatPacks = () => {
-  return <div>AdminBeatPacks</div>;
-};
-
-export default AdminBeatPacks;
